3ae12d6d337780fb05d3b9a547c80d7a57508d22
Aliihsan Yilmaz <97608779+Aliihsany@users.noreply.github.com>
2024-05-22 11:09:49 +0200

    structuren

--- comfort-living/src/App.js
+++ comfort-living/src/App.js
@@ -1,11 +1,39 @@
-import './App.css';
+import React from 'react';
+import Homepage from './components/Homepage'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      
+ 
+ <header>
+      <div className="logo">Logo van website</div>
+      <nav>
+        <a href="#">Ik huur</a>
+        <a href="#">Ik zoek</a>
+        <a href="#">Organisatie</a>
+        <a href="#">Contact</a>
+        <input type="text" placeholder="Search" />
+        <a href="#">Inloggen</a>
+      </nav>
+    </header>
+
+
+      <Homepage />
+
+      <footer>
+      <div className="customer-service">
+        <a href="#">Contact</a>
+        <a href="#">Veelgestelde vragen</a>
+      </div>
+      <div className="social-media">
+        <a href="#">Facebook</a>
+        <a href="#">Instagram</a>
+        <a href="#">Twitter</a>
+      </div>
+    </footer>
     </div>
+    
   );
-}
+};
 
 export default App;
---
+++ comfort-living/src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="about">
+      <p>
+        Wij verhuren 9,000 betaalbare woningen in de gemeenten Elburg, Ermelo,
+        Harderwijk en Oldebroek.
+      </p>
+      <p>KvK-nummer: 08012356</p>
+    </div>
+  );
+};
+
+export default About;
