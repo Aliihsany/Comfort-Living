d52490d9366133e791db7620dc9f5dc5e67d6f9a
AyyoubN <Najimayoub76@gmail.com>
2024-05-28 15:08:14 +0200

    Update Unauthorized.js
    homepage button

--- comfort-living/src/components/Unauthorized.js
+++ comfort-living/src/components/Unauthorized.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Unauthorized = () => {
+  const navigate = useNavigate();
+
+  const goToHomePage = () => {
+    navigate('/');
+  };
+
   return (
     <div>
       <h1>Unauthorized</h1>
       <p>You do not have permission to access this page.</p>
+      <button onClick={goToHomePage}>Go to Home Page</button>
     </div>
   );
 };
